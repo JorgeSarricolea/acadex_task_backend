1cd73ffebf700e7099cc16d9d5d13ca3d4536887
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-09-29 04:15:21 -0600

    feat(adapter): create database adapter for User
        - Implemented UserDatabaseAdapter to manage user data interactions.
        - Added methods to create, retrieve, update, and delete users in the database.
        - Utilized Prisma client for database operations.

---
+++ src/adapters/database/UserDatabaseAdapter.js
@@ -0,0 +1,28 @@
+import prisma from "../../infrastructure/database/prisma.js";
+
+export class UserDatabaseAdapter {
+  async createUser(userData) {
+    return await prisma.user.create({
+      data: userData,
+    });
+  }
+
+  async getUserByEmail(email) {
+    return await prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async updateUser(userId, updateData) {
+    return await prisma.user.update({
+      where: { id: userId },
+      data: updateData,
+    });
+  }
+
+  async deleteUser(userId) {
+    return await prisma.user.delete({
+      where: { id: userId },
+    });
+  }
+}
