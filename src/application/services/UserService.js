6a3af3ede7cb6ba1813c07ab1ad408712919ef40
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-10-02 13:04:38 -0600

    feat(user-service): create UserService to handle user-related operations
        - Added 'UserService' to centralize user-related logic and operations.
        - Implemented 'checkIfEmailExists' to verify if an email is already associated with a different user.
        - Added 'getUserById' method to retrieve user details by ID with proper error handling.
        - Created 'updateUser' method to update user data, with validation for non-existent users.

---
+++ src/application/services/UserService.js
@@ -0,0 +1,48 @@
+import { ErrorFactory } from "./ErrorFactory.js";
+
+export default class UserService {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  /**
+   * Check if the email already exists for a different user.
+   * @param {string} email - The email to be checked.
+   * @param {string} [userId] - Optional userId to exclude from the check.
+   * @throws Will throw an error if the email already exists for another user.
+   */
+  async checkIfEmailExists(email, userId = null) {
+    const existingUser = await this.userRepository.getUserByEmail(email);
+    if (existingUser && existingUser.id !== userId) {
+      throw ErrorFactory.createError("Validation", {
+        message: "Email already in use by another user",
+        details: `User with email ${email} already exists`,
+      });
+    }
+  }
+
+  async getUserById(userId) {
+    const user = await this.userRepository.getById(userId);
+    if (!user) {
+      throw ErrorFactory.createError("NotFound", {
+        resource: "User",
+        details: `User with ID ${userId} not found`,
+      });
+    }
+    return user;
+  }
+
+  async updateUser(userId, updateData) {
+    const updatedUser = await this.userRepository.updateUser(
+      userId,
+      updateData
+    );
+    if (!updatedUser) {
+      throw ErrorFactory.createError("NotFound", {
+        resource: "User",
+        details: `User with ID ${userId} not found`,
+      });
+    }
+    return updatedUser;
+  }
+}
